bf47a0d41f6db613cf6797fb2165eaed1689f437
KantJian3 <90332976+KantJian@users.noreply.github.com>
2024-06-17 22:30:46 +0800

    Add files via upload

---
+++ Simulate_elevator_operation/plan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //模拟电梯运行
+            //业主    电梯    楼层  
+        }
+    }
+}
