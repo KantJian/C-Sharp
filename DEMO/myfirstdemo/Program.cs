3af41a0a73c037c21485acad1675c2dfd9eac26f
KantJian3 <90332976+KantJian@users.noreply.github.com>
2024-06-17 22:16:21 +0800

    Add files via upload

---
+++ DEMO/myfirstdemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfirstdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hwllo,World!");
+            Console.ReadKey();
+            Console.WriteLine("我也是Hello World"); 
+            Console.ReadKey();
+            Console.WriteLine("666666");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DEMO/myseconddemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myseconddemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //这行代码的作用是将this---demo打印到控制台当中
+            Console.WriteLine("This Is my Second Demo");
+            //这行代码是暂停当前这个程序
+            Console.ReadKey();
+
+
+            #region 注释练习加折叠
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            //Console.WriteLine("This Is my Second Demo");
+            #endregion
+        }
+
+
+
+
+    }
+}
