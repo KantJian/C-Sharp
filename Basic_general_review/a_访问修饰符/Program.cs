98716ee76e57a1e06e3bf11212174766c6d1f9ea
KantJian3 <90332976+KantJian@users.noreply.github.com>
2024-06-17 22:02:34 +0800

    Add files via upload

---
+++ Basic_general_review/a_访问修饰符/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_访问修饰符
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Basic_general_review/a_面向对象复习/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_面向对象复习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this
+            //new
+            //1):创建对象
+            //2):隐藏从父类那里继承过来的成员
+        }
+    }
+
+    public class Person
+    {
+        public void T()
+        {
+
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public new void T()
+        {
+
+        }
+    }
+}
